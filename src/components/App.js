c08cd3c77c831dfec7ba2414ffbe2aba88386c34
El Rafa <rafaelrosas19@gmail.com>
2021-02-06 13:21:54 -0600

    Display roster on page

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,16 @@
 import "../App.css";
 import Welcome from "./Welcome";
+import { playerData } from "../data/playerData";
+import Player from "./Player";
+import PlayerList from "./PlayerList";
+
+// console.log("pd", playerData);
 
 function App() {
   return (
     <div className="container">
       <Welcome />
+      <PlayerList players={playerData} />
     </div>
   );
 }
